5e46b2d1cc59b16fffb01d475417cc3a8e1a95f6
unknown <nicolas.pachon-u@labinfo.is.escuelaing.edu.co>
2025-09-09 11:19:41 -0500

    add: progress made of exam

--- src/main/java/com/mycompany/parcial/Parcial.java
+++ src/main/java/com/mycompany/parcial/Parcial.java
@@ -1,16 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.parcial;
 
-/**
- *
- * @author nicolas.pachon-u
- */
+import java.net.*;
+import java.io.*;
+
 public class Parcial {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final String USER_AGENT = "Mozilla/5.0";
+    private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(5000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 5000.");
+            System.exit(1);
+        }
+
+        Socket clientSocket = null;
+        try {
+            System.out.println("Listo para recibir ...");
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            System.err.println("Accept failed.");
+            System.exit(1);
+        }
+        URL obj = new URL(GET_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        
+        
+        PrintWriter out = new PrintWriter(
+                clientSocket.getOutputStream(), true);
+        StringBuffer response = new StringBuffer();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(clientSocket.getInputStream()));
+        String inputLine, outputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println("Recibi: " + inputLine);
+            response.append(inputLine);
+            if (!in.ready()) {
+                break;
+            }
+        }
+        outputLine = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "<head>\n"
+                + "<meta charset=\"UTF-8\">\n"
+                + "<title> Key check</title>\n"
+                + "</head>\n"
+                + "<body>\n"
+                + "<h1>Pending key value</h1>\n"
+                + "</body>\n"
+                + "</html>\n";
+
+        out.println(outputLine);
+        out.close();
+        in.close();
+        clientSocket.close();
+        serverSocket.close();
     }
 }
